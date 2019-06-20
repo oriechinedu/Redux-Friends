d8e99967f858c263aaa7549e1bb8d5003ffff8a5
Orie Chinedu <oriechinedu@gmail.com>
2019-06-20 13:47:41 +0100

    Create reducer

---
+++ friends/src/components/store/reducer/index.js
@@ -0,0 +1,46 @@
+import * as types from '../actions/types'
+
+const initialState = {
+  deletingFriend: false,
+  fetchingFriends: false,
+  friends: [],
+  loggingIn: false,
+  savingFriends: false,
+  updatingFriend: false,
+  isLoading: false,
+  error: null
+}
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case types.REQUESTING:
+      return {
+        ...state,
+        isLoading: true,
+      }
+    case types.FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      }
+    case types.LOGIN_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+      }
+    case types.FETCHING_FRIENDS_SUCCESS:
+      return {
+        ...state,
+        friends: action.payload
+      }
+    case types.SAVING_FRIENDS_SUCCESS:
+      return {
+        ...state,
+        friends: action.payload
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer;
